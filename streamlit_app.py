981563ac08d076e42fc1e3f6d92384f96093129e
RyuKangHyeon <162665744+RyuKangHyeon@users.noreply.github.com>
2024-03-27 01:16:21 +0000

    Wrong text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 rand = np.random.normal(1, 2, size=20)
-fig, ax = plt.subpots()
+fig, ax = plt.subpot()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
